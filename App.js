9d93091ba452ff770c48f789940ffb645eb62827
Ajaykumar Rajasekaran <ajaykumarrajasekaran@Ajaykumars-MacBook-Pro.local>
2023-03-17 14:49:13 +0530

    added LogBox

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import './src/api/MockServer'
 import RootNavigation from "./src/navigation";
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
 
 export default function App() {
   return <RootNavigation></RootNavigation>;
